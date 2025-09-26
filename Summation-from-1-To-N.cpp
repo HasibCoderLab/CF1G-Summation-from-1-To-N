d91042e3c3d1cb1276d073f15cdde44e2f6a1aff
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-26 17:16:50 +0600

    file renmae

---
+++ Summation-from-1-To-N.cpp
@@ -0,0 +1,8 @@
+//  ================================================
+//                Platform : Codeforces
+//                Contest  : CF1G
+//                Problem  : Summation-from-1-To-N
+//                Language : C++
+//                Topic    : [Summation-from-1-To-N]
+//                File     : Summation-from-1-To-N.cpp
+// ================================================
