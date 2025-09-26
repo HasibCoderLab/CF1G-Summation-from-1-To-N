72bcc4ea9aa6254d66bad94ccdfb71e13b94adc9
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-26 17:24:03 +0600

    solving ..

--- Summation-from-1-To-N.cpp
+++ Summation-from-1-To-N.cpp
@@ -6,3 +6,18 @@
 //                Topic    : [Summation-from-1-To-N]
 //                File     : Summation-from-1-To-N.cpp
 // ================================================
+#include <iostream>
+using namespace std;
+int main()
+{
+   double N ;
+   cin>>N;
+   double sum = 0;
+   for (int i = 0; i <=N; i++)
+   {
+    sum +=i;
+    cout<< sum;
+   }
+   
+    return 0;
+}
